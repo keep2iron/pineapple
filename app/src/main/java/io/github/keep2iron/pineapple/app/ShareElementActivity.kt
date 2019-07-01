b1f8c110256079983991e43f1acbdbc46ebe5d9f
keep2iron <497881309@qq.com>
2019-07-01 10:40:40 +0800

    1.修改share element activity的编译错误 2.修改min sdk

--- app/src/main/java/io/github/keep2iron/pineapple/app/ShareElementActivity.kt
+++ app/src/main/java/io/github/keep2iron/pineapple/app/ShareElementActivity.kt
@@ -1,5 +1,6 @@
 package io.github.keep2iron.pineapple.app
 
+import android.os.Build
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.facebook.drawee.drawable.ScalingUtils
@@ -13,17 +14,21 @@ class ShareElementActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_share_element)
-        window.sharedElementEnterTransition = DraweeTransition.createTransitionSet(
-            ScalingUtils.ScaleType.CENTER_CROP,
-            ScalingUtils.ScaleType.FIT_CENTER
-        ) // 进入
-        window.sharedElementReturnTransition = DraweeTransition.createTransitionSet(
-            ScalingUtils.ScaleType.FIT_CENTER,
-            ScalingUtils.ScaleType.CENTER_CROP
-        ) // 返回
 
         val imageView = findViewById<MiddlewareView>(R.id.imageView)
-        imageView.transitionName = "imageView"
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.sharedElementEnterTransition = DraweeTransition.createTransitionSet(
+                ScalingUtils.ScaleType.CENTER_CROP,
+                ScalingUtils.ScaleType.FIT_CENTER
+            )// 进入
+            window.sharedElementReturnTransition = DraweeTransition.createTransitionSet(
+                ScalingUtils.ScaleType.FIT_CENTER,
+                ScalingUtils.ScaleType.CENTER_CROP
+            ) // 返回
+            imageView.transitionName = "imageView"
+        }
+
         val url = intent.getStringExtra("url")
         ImageLoaderManager.getInstance().showImageView(
             imageView, url
