16e6530ef303a8907d7af0badf2500f240872ce8
keep2iron <497881309@qq.com>
2020-02-25 10:56:43 +0800

    0.0.2发布

--- app/src/main/java/io/github/keep2iron/pineapple/app/GlideModule.kt
+++ app/src/main/java/io/github/keep2iron/pineapple/app/GlideModule.kt
@@ -1,7 +1,7 @@
 package io.github.keep2iron.pineapple.app
 
-import com.bumptech.glide.annotation.GlideModule
-import io.github.keep2iron.pineapple.GlideInitModule
-
-@GlideModule
-class GlideModule : GlideInitModule()
+//import com.bumptech.glide.annotation.GlideModule
+//import io.github.keep2iron.pineapple.GlideInitModule
+//
+//@GlideModule
+//class GlideModule : GlideInitModule()
